d99ecf230328ecf0afc3ed87d662d518eda7bfda
lilei <lilei03@renrenche.com>
2019-02-13 17:07:38 +0800

    fix(remove yarn):

--- index.js
+++ index.js
@@ -1,82 +1,45 @@
 const fs = require('fs')
-const path = require('path')
 const ora = require('ora')
+const path = require('path')
 const chalk = require('chalk')
 const execa = require('execa')
-const { execSync } = require('child_process')
 
-const cwd = path.resolve(process.cwd(), '../../')
-const pkgFile = path.resolve(cwd, './package.json')
-const lockFile = path.resolve(cwd, 'yarn.lock')
-const isYarn = !!(hasYarn() && fs.existsSync(lockFile))
+const rootpath = path.resolve(process.cwd(), '../../')
+const pkgpath = path.resolve(rootpath, './package.json')
 
 const spinner = ora('Loading devDependencies...')
 
-async function shell(sh, global = false) {
-  let she
-  if (isYarn) {
-    she = `yarn ${global ? 'global ' : ''}add ${sh}`
-  } else {
-    she = `npm install ${sh} ${global ? '-g' : ''}`
-  }
+async function shell(sh) {
   console.log('')
-  console.log(chalk.red(she))
-  return execa.shell(she, { cwd })
-}
-
-function hasYarn() {
-  try {
-    execSync('yarn --version', { stdio: 'ignore' })
-    return true
-  } catch (error) {
-    return false
-  }
+  console.log(chalk.red(sh))
+  return execa.shell(sh, {cwd: rootpath})
 }
 
 async function install() {
-  try {
-    spinner.start()
-    // commitizen
-    await shell('commitizen', true)
-    if (isYarn) {
-      await execa.shell('commitizen init cz-conventional-changelog --yarn --dev --exact', { cwd })
-    } else {
-      await execa.shell('commitizen init cz-conventional-changelog -D -E', { cwd })
-    }
-    spinner.text = 'commitizen installed'
-  
-    // commitlint
-    if (process.platform === 'win32') {
-      if (isYarn) {
-        await shell('@commitlint/config-conventional @commitlint/cli --dev')
-      } else {
-        await shell('@commitlint/config-conventional @commitlint/cli -D')
-      }
-    } else {
-      if (isYarn) {
-        await shell('@commitlint/{config-conventional,cli} --dev')
-      } else {
-        await shell('@commitlint/{config-conventional,cli} -D')
-      }
-    }
-    spinner.text = 'commitlint installed'
-  
-    await execa.shell('echo module.exports = {extends: [\'@commitlint/config-conventional\']} > commitlint.config.js', { cwd })
-  
-    // lint-staged
-    await shell('husky lint-staged -D')
+  spinner.start()
+  // commitizen
+  await shell('npm install -g commitizen')
+  await shell('commitizen init cz-conventional-changelog --save-dev --save-exact')
+  spinner.text = 'commitizen installed'
 
-    spinner.succeed(chalk.green('devDependencies installed'))
-  } catch (err) {
-    console.log(err)
-    process.exit(1)
+  // commitlint
+  if (process.platform === 'win32') {
+    await shell('npm install --save-dev @commitlint/config-conventional @commitlint/cli')
+  } else {
+    await shell('npm install --save-dev @commitlint/{config-conventional,cli}')
   }
+  spinner.text = 'commitlint installed'
+
+  await shell('echo module.exports = {extends: [\'@commitlint/config-conventional\']} > commitlint.config.js')
+
+  // lint-staged
+  await shell('npm install husky lint-staged -D')
+  spinner.succeed(chalk.green('devDependencies installed'))
 }
 
 async function writeFile() {
   spinner.text = 'write file...'
-
-  fs.readFile(pkgFile, 'utf8', (err, data) => {
+  fs.readFile(pkgpath, 'utf8', (err, data) => {
     if (err) {
       console.log(err)
       spinner.fail('failed there has no package.json you should run npm init on the command')
@@ -97,7 +60,7 @@ async function writeFile() {
 
     const newPkg = JSON.stringify(pkg, null, 2)
 
-    fs.writeFile(pkgFile, newPkg, 'utf8', err => {
+    fs.writeFile(pkgpath, newPkg, 'utf8', err => {
       if (err) {
         console.error(err)
       }
@@ -107,9 +70,8 @@ async function writeFile() {
 }
 
 async function init() {
-  if (!fs.existsSync(pkgFile)) {
-    console.log('No such package.json File init the package.json')
-    execa.shellSync('npm init --yes')
+  if (!fs.existsSync(pkgpath)) {
+    execa.shellSync('npm init --yes', {cwd: rootpath})
   }
   await install()
   await writeFile()
